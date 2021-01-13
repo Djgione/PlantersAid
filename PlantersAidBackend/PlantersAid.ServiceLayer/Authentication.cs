45a0aeb82050a9ab297b59228218a678899d978e
Djgione <thegiones13@sbcglobal.net>
2021-01-13 01:02:42 -0800

    Added Constructor to Authentication, Created Property for IAccountDAO

--- PlantersAidBackend/PlantersAid.ServiceLayer/Authentication.cs
+++ PlantersAidBackend/PlantersAid.ServiceLayer/Authentication.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PlantersAid.DataAccessLayer.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,13 @@ namespace PlantersAid.ServiceLayer
 {
     public class Authentication
     {
-        
+        public IAccountDAO DataAccess { get; }
+        public Authentication (IAccountDAO dataAccess)
+        {
+            DataAccess = dataAccess;
+        }
+
+
 
 
     }
