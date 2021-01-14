42b293b5095d947bf574d8de901d0ae043a3f565
Djgione <thegiones13@sbcglobal.net>
2021-01-14 01:33:06 -0800

    Started Creating AccountSqlDAO

---
+++ PlantersAidBackend/PlantersAid.DataAccessLayer/AccountSqlDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlantersAid.DataAccessLayer.Interfaces;
+using PlantersAid.Models;
+
+namespace PlantersAid.DataAccessLayer
+{
+    public class AccountSqlDAO : IAccountDAO
+    {
+        /// <summary>
+        /// SQL Method Implementation for Deleting Account
+        /// </summary>
+        /// <returns></returns>
+        public AccessResult DeleteAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// SQL Method Implementation for Retreiving Account from DB
+        /// </summary>
+        /// <returns></returns>
+        public Account GetAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// SQL Method Implementation for Changing Password
+        /// </summary>
+        /// <returns></returns>
+        public AccessResult ChangePassword()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PlantersAidBackend/PlantersAid.DataAccessLayer/Interfaces/IAccountDAO.cs
+++ PlantersAidBackend/PlantersAid.DataAccessLayer/Interfaces/IAccountDAO.cs
@@ -4,11 +4,29 @@ using System.Text;
 using PlantersAid.Models;
 namespace PlantersAid.DataAccessLayer.Interfaces
 {
+    /// <summary>
+    /// Interface for Accessing all Data Relating to Accounts
+    /// </summary>
     public interface IAccountDAO
     {
+        /// <summary>
+        /// Method for Retrieving Account from DB
+        /// </summary>
+        /// <returns></returns>
         public Account GetAccount();
 
+        /// <summary>
+        /// Method for Deleting Account for DB
+        /// </summary>
+        /// <returns></returns>
         public AccessResult DeleteAccount();
+
+
+        /// <summary>
+        /// Method for Changing Password
+        /// </summary>
+        /// <returns></returns>
+        public AccessResult ChangePassword();
 
     }
 }
