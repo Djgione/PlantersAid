3b98d6f844742d41cfc768a388aac266a71a84bf
Djgione <thegiones13@sbcglobal.net>
2021-01-17 18:38:43 -0800

    Created Email System to Message from Official Email

---
+++ PlantersAidBackend/PlantersAid.ServiceLayer/Email.cs
@@ -0,0 +1,49 @@
+﻿using PlantersAid.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantersAid.ServiceLayer
+{
+    public class Email
+    {
+        /// <summary>
+        /// Email Sending, Can be Used in Conjunction with MailMessageBuilder
+        /// </summary>
+        
+        public static Result SendEmail(MailMessage email)
+        {
+            var creds = new NetworkCredential("plantersaid@gmail.com", Environment.GetEnvironmentVariable("plantersAidEmailPassword"));
+            Result result;
+            var client = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = creds
+            };
+
+            try
+            {
+                client.Send(email);
+                result = new Result(true, "Message Sent!");
+            }
+            catch (Exception e)
+            {
+                result = new Result(false, e.ToString());
+            }
+
+            email.Dispose();
+            return result;
+        }
+
+        
+    }
+}
---
+++ PlantersAidBackend/PlantersAid.ServiceLayer/MailMessageBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantersAid.ServiceLayer
+{
+    public class MailMessageBuilder
+    {
+        MailAddress ToAddress;
+        string Subject;
+        string Body;
+        readonly MailAddress FromAddress;
+
+
+        public MailMessageBuilder()
+        {
+            FromAddress = new MailAddress("plantersaid@gmail.com");
+        }
+
+        public MailMessageBuilder SetToAddress(String address)
+        {
+            ToAddress = new MailAddress(address);
+            return this;
+        }
+
+        public MailMessageBuilder SetSubject(String subject)
+        {
+            Subject = subject;
+            return this;
+        }
+
+        public MailMessageBuilder SetBody(String body)
+        {
+            Body = body;
+            return this;
+        }
+
+        public MailMessage Build()
+        {
+            var message = new MailMessage(FromAddress, ToAddress);
+            message.Subject = Subject;
+            message.Body = Body;
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.SubjectEncoding = System.Text.Encoding.UTF8; 
+            return message;
+        }
+        
+    }
+}
